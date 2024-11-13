5fa627819293a2e87a74e20342a3fa7465110737
ZHAW <zhaw@czudam-MacBook.local>
2024-11-13 10:58:21 +0100

    fix gefahr

--- src/app/appointment-form/appointment-form.component.ts
+++ src/app/appointment-form/appointment-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-appointment-form',
@@ -40,22 +41,35 @@ export class AppointmentFormComponent implements OnInit {
   constructor(private sharedService: SharedService) {}
 
   ngOnInit() {
-    this.sharedService.currentScoreTemp.subscribe(scoreTemp => this.scoreTemp = scoreTemp);
-    this.sharedService.currentScorePrecipitation.subscribe(scorePrec => this.scorePrec = scorePrec);
-    this.sharedService.currentScoreRain.subscribe(scoreRain => this.scoreRain = scoreRain);
-    this.sharedService.currentScoreSnowfall.subscribe(scoreSnow => this.scoreSnow = scoreSnow);
-    this.sharedService.currentScoreSnowDepth.subscribe(scoreSnowDepth => this.scoreSnowDepth = scoreSnowDepth);
-    this.sharedService.currentScoreWind10m.subscribe(scoreWind10m => this.scoreWind10m = scoreWind10m);
-    this.sharedService.currentScoreWind100m.subscribe(scoreWind100m => this.scoreWind100m = scoreWind100m);
-    this.sharedService.currentScoreWindGust.subscribe(scoreWindGust => this.scoreWindGust = scoreWindGust)
-    this.sharedService.currentDangerTemp.subscribe(scoreTemp => {this.dangerTemp = this.getDanger(this.scoreTemp);});
-    this.sharedService.currentDangerPrec.subscribe(scorePrec => {this.dangerPrec = this.getDanger(this.scorePrec);});
-    this.sharedService.currentDangerRain.subscribe(scoreRain => {this.dangerRain = this.getDanger(this.scoreRain);});
-    this.sharedService.currentDangerSnow.subscribe(scoreSnow => {this.dangerSnow = this.getDanger(this.scoreSnow);});
-    this.sharedService.currentDangerSnowDepth.subscribe(scoreSnowDepth => {this.dangerSnowDepth = this.getDanger(this.scoreSnowDepth);});
-    this.sharedService.currentDangerWind10.subscribe(scoreWind10m => {this.dangerWind10 = this.getDanger(this.scoreWind10m);});
-    this.sharedService.currentDangerWind100.subscribe(scoreWind100m => {this.dangerWind100 = this.getDanger(this.scoreWind100m);});
-    this.sharedService.currentDangerGust.subscribe(scoreWindGust => {this.dangerGust = this.getDanger(this.scoreWindGust);})    
+    combineLatest([
+      this.sharedService.currentScoreTemp,
+      this.sharedService.currentScorePrecipitation,
+      this.sharedService.currentScoreRain,
+      this.sharedService.currentScoreSnowfall,
+      this.sharedService.currentScoreSnowDepth,
+      this.sharedService.currentScoreWind10m,
+      this.sharedService.currentScoreWind100m,
+      this.sharedService.currentScoreWindGust
+    ]).subscribe(([scoreTemp, scorePrec, scoreRain, scoreSnow, scoreSnowDepth, scoreWind10m, scoreWind100m, scoreWindGust]) => {
+      this.scoreTemp = scoreTemp;
+      this.scorePrec = scorePrec;
+      this.scoreRain = scoreRain;
+      this.scoreSnow = scoreSnow;
+      this.scoreSnowDepth = scoreSnowDepth;
+      this.scoreWind10m = scoreWind10m;
+      this.scoreWind100m = scoreWind100m;
+      this.scoreWindGust = scoreWindGust;
+  
+      // Update the danger levels
+      this.dangerTemp = this.getDanger(this.scoreTemp);
+      this.dangerPrec = this.getDanger(this.scorePrec);
+      this.dangerRain = this.getDanger(this.scoreRain);
+      this.dangerSnow = this.getDanger(this.scoreSnow);
+      this.dangerSnowDepth = this.getDanger(this.scoreSnowDepth);
+      this.dangerWind10 = this.getDanger(this.scoreWind10m);
+      this.dangerWind100 = this.getDanger(this.scoreWind100m);
+      this.dangerGust = this.getDanger(this.scoreWindGust);
+    });
   }
 
 }
